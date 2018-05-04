2c95a675c17ee8fa6fe953c12186f5babc5cd26a
aurora <18862324237@163.com>
2018-05-04 17:44:47 +0800

    user can register using phone

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -12,17 +12,21 @@ class VerificationCodesController extends Controller
     {
         $phone = $request->phone;
 
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
-        try {
-            $result = $easySms->send($phone, [
-                'content' => "【Lbbs社区】您的验证码是 {$code}。如非本人操作，请忽略本短信"
-            ]);
-        } catch (\GuzzleHttp\Exception\ClientException $exception) {
-            $response = $exception->getResponse();
-            $result = json_decode($response->getBody()->getContents(), true);
-            return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+            try {
+                $result = $easySms->send($phone, [
+                    'content' => "【Lbbs社区】您的验证码是 {$code}。如非本人操作，请忽略本短信"
+                ]);
+            } catch (\GuzzleHttp\Exception\ClientException $exception) {
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+            }
         }
 
         $key = 'verificationCode_' . str_random(15);
