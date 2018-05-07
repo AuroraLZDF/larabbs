12a1644b20d156d864b879123092352080b1f113
aurora <18862324237@163.com>
2018-05-07 11:05:45 +0800

    add captcha

--- routes/api.php
+++ routes/api.php
@@ -32,5 +32,8 @@ $api->group(['domain' => domain(config('app.bbs_url')), 'namespace' => 'App\Http
         // 用户注册
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
+        // 图片验证码
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
     });
 });
