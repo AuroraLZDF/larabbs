a6d28af67ba38841d99279f131e51127e04276fe
aurora <18862324237@163.com>
2018-08-30 13:34:34 +0800

    111

--- app/Models/Bbs/User.php
+++ app/Models/Bbs/User.php
@@ -121,11 +121,21 @@ class User extends  Authenticatable implements JWTSubject
         return $model->paginate($limit, ['*'], 'page', $page);
     }
 
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
     public function getJWTCustomClaims()
     {
         return [];
